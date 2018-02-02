b8381f5d773059445d7889174430b32378098270
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-02-02 15:19:51 +0530

    SMS commented

--- Hiru_AlphaKinesis/lambda02.js
+++ Hiru_AlphaKinesis/lambda02.js
@@ -1,28 +1,28 @@
 let AWS = require('aws-sdk');
 const sns = new AWS.SNS();
 exports.handler = function (event, context, callback) {
-	sns.publish({
-		Message: 'this is a test message',
-		MessageAttributes: {
-			'AWS.SNS.SMS.SMSType': {
-				DataType: 'String',
-				StringValue: 'Transactional'
-			},
-			'AWS.SNS.SMS.SenderID': {
-				DataType: 'String',
-				StringValue: 'Hirudinee'
-			}
-		},
-		PhoneNumber: '+94715397214'
-	}).promise()
-		.then(data => {
+	// sns.publish({
+	// 	Message: 'this is a test message',
+	// 	MessageAttributes: {
+	// 		'AWS.SNS.SMS.SMSType': {
+	// 			DataType: 'String',
+	// 			StringValue: 'Transactional'
+	// 		},
+	// 		'AWS.SNS.SMS.SenderID': {
+	// 			DataType: 'String',
+	// 			StringValue: 'Hirudinee'
+	// 		}
+	// 	},
+	// 	PhoneNumber: '+94715397214'
+	// }).promise()
+	// 	.then(data => {
 			
-			console.log("successful , data : ",data);
-		})
-		.catch(err => {
-			console.log("Error , error message : ",err);
+	// 		console.log("successful , data : ",data);
+	// 	})
+	// 	.catch(err => {
+	// 		console.log("Error , error message : ",err);
 			
-		});
+	// 	});
 	sns.createPlatformEndpoint({
 		PlatformApplicationArn: 'arn:aws:sns:us-east-1:480964559519:app/BAIDU/Test-Baidu-App',
 		Token: '123456',
